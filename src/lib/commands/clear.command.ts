68f6a5628fd36edced945344ad5cd5b5c67043c1
Kirill <kimerland@gmail.com>
2025-06-21 18:51:38 +0300

    Clear Fn

--- src/lib/commands/clear.command.ts
+++ src/lib/commands/clear.command.ts
@@ -2,6 +2,23 @@ import { prisma } from 'prisma/prisma.service';
 import { Context } from 'telegraf';
 
 export async function clearCommand(ctx: Context) {
-  await prisma.suggestion.deleteMany();
-  await ctx.reply('Все сообщения удалены!');
+  const suggestions = await prisma.suggestion.findMany({
+    where: { chatId: ctx.from?.id.toString() },
+  });
+
+  for (const suggestion of suggestions) {
+    try {
+      await ctx.telegram.deleteMessage(suggestion.chatId, suggestion.messageId);
+    } catch (err) {
+      console.warn(
+        `Не удалось удалить сообщение ${suggestion.messageId}:`,
+        err.message,
+      );
+    }
+  }
+
+  await prisma.suggestion.deleteMany({
+    where: { chatId: ctx.from?.id.toString() },
+  });
+  await ctx.reply(`✅ Удалено сообщений из чата: ${suggestions.length}`);
 }
--- src/lib/user.handler.ts
+++ src/lib/user.handler.ts
@@ -32,41 +32,26 @@ export const handleUserMessage = async (ctx: Context, adminId: string) => {
   ID: ${from.id}
   Имя: ${from.first_name} ${from.last_name || ''}`;
 
-  if ('text' in message) {
-    await ctx.telegram.sendMessage(
-      adminId,
-      `${userInfo}\n\n Сообщение: \n${message.text}`,
-      {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: 'Ответить',
-                callback_data: `reply:${from.id}`,
-              },
-            ],
-          ],
-        },
-      },
-    );
-  }
-
-  if ('photo' in message) {
-    const photo = message.photo[message.photo.length - 1].file_id;
-    await ctx.telegram.sendPhoto(adminId, photo, {
-      caption: userInfo,
+  const sent = await ctx.telegram.sendMessage(
+    adminId,
+    `${userInfo}\n ${text}`,
+    {
       reply_markup: {
         inline_keyboard: [
-          [
-            {
-              text: 'Ответить',
-              callback_data: `reply:${from.id}`,
-            },
-          ],
+          [{ text: 'Ответить', callback_data: `reply:${from.id}` }],
         ],
       },
-    });
-  }
+    },
+  );
+
+  await prisma.suggestion.create({
+    data: {
+      userId,
+      chatId: adminId,
+      messageId: sent.message_id,
+      text: text || '',
+    },
+  });
 
   await ctx.reply('Спасибо за ваше сообщение!');
 };
